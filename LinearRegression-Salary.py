9e880eafb8568a201fb4caad272f6415574c5e5c
Sri Teja Mamidi <98144154+SriTejaMamidi@users.noreply.github.com>
2026-02-03 13:36:29 +0530

    Made changes like using pipeline,columtransformer

--- LinearRegression-Salary.py
+++ LinearRegression-Salary.py
@@ -1,45 +1,50 @@
-#Importing the required modules
-import numpy as np
 import pandas as pd
-#Importing data into df
-df=pd.read_csv('Salary_Data[1].csv')
-#Checking head and null values
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score,root_mean_squared_error
+
+df=pd.read_csv("Salary_Data[1].csv")
 print(df.head())
 print(df.info())
-#Filling the null values
-df['Education Level'].fillna(df['Education Level'].mode()[0],inplace=True)
-df['Job Title'].fillna(df['Job Title'].mode()[0],inplace=True)
-df['Gender'].fillna(df['Gender'].mode()[0],inplace=True)
-df['Years of Experience'].fillna(df['Years of Experience'].mode()[0],inplace=True)
-df['Salary'].fillna(df['Salary'].median(),inplace=True)
-df['Age'].fillna(df['Age'].median(),inplace=True)
-#Rechecking if there are null values
+print(df.describe())
+print(df.isnull().sum())
+df['Salary']=df['Salary'].fillna(df['Salary'].median())
+num_cols=['Age','Years of Experience']
+df[num_cols]=df[num_cols].fillna(df[num_cols].median())
+cat_cols=['Gender','Education Level','Job Title']
+df[cat_cols]=df[cat_cols].fillna(df[cat_cols].mode().iloc[0])
+#Checking again if there are any null values
 print(df.info())
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error,r2_score
-#Encoding categorical values
-df_encoded=pd.get_dummies(df,columns=['Job Title','Gender','Education Level'],drop_first=True)
-#Featuring inputs and output
-x=df_encoded.drop('Salary',axis=1)
-y=df_encoded['Salary']
-#Train test split
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=48)
-scaler=StandardScaler()
-x_train_scaled=scaler.fit_transform(x_train)
-x_test_scaled=scaler.transform(x_test)
-#Linear Regression
-model=LinearRegression()
-model.fit(x_train_scaled,y_train)
-#Evalulating the model
-y_pred=model.predict(x_test_scaled)
+#Giving input and outputs
+x=df.drop('Salary',axis=1)
+y=df['Salary']
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+preprocessor=ColumnTransformer(
+    transformers=[
+    ('cat',OneHotEncoder(handle_unknown='ignore'),cat_cols),
+    ('num','passthrough',num_cols)
+])
+model=Pipeline(steps=[
+    ('preprocessor',preprocessor),
+    ('mod',LinearRegression())
+    ])
+model.fit(x_train,y_train)
+y_pred=model.predict(x_test)
 mse=mean_squared_error(y_test,y_pred)
 r2=r2_score(y_test,y_pred)
-print(mse,r2)
-#Predict new salary
-new_experience_prediction=np.array([[12,35]])
-new_experience_scaled=scaler.transform(new_experience_prediction)
-prediction=model.predict(new_experience_scaled)
-print("\nPredicted salary is",prediction)
-
+rmse=root_mean_squared_error(y_test,y_pred)
+print("MSE:",mse)
+print("R2:",r2)
+print("RMSE:",rmse)
+new_data=pd.DataFrame({
+    'Age':[35],
+    'Years of Experience':[8],
+    'Gender':['Male'],
+    'Education Level':['Bachelors'],
+    'Job Title':['Software Engineer']
+})
+predicted_salary=model.predict(new_data)
+print(predicted_salary)
